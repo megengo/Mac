ad719cbf46e129e18bebb00aa4fc84becb46dbf7
megengo <megengo@gmail.com>
2023-11-17 23:14:31 -0600

    message

--- mcl/mcl.zsh
+++ mcl/mcl.zsh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 #  mcf.sh
 #  
@@ -6,6 +6,13 @@
 #  Created by Chris Cox on 11/17/23.
 #  
 # Example script which sources mcf to demonstrate capability
+
 source mcl
-mclDebug "This is the first debug message ever"
-mclFailChk -p 0 "This message is a false negative demonstrating the -p option"
+mclDebug "$0: source mcl: $?"
+mclInfo "Mac Core Library Version: $MCL_VERSION"
+
+false
+mclFDebug "Fail Check Debug Message"
+mclFInfo "Fail Check Info Message"
+mclFWarn "Fail Check Warn Message"
+mclFError "Fail Check Error Message"
